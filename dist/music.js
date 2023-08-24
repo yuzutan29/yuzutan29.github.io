4e304124a523b0ba4f7f76811a6173b9e5540ae6
yuzutan29 <284743840@qq.com>
2023-08-24 15:13:17 +0800

    Site updated: 2023-08-24 15:13:17

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: 'ヒカリヘ (向着光) (piano version)',
+        artist: '林友树 (林ゆうき)',
+        url: 'https://y.qq.com/#type=song&mid=003g4pMq16jlC7&tpl=yqq_song_detail',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000000ECmTF22VO0h.jpg',
+      }
+    ]
+});
