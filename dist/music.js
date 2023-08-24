5835308e0f1323df7889ee7fb5574de687b9181f
yuzutan29 <284743840@qq.com>
2023-08-25 00:21:22 +0800

    Site updated: 2023-08-25 00:21:22

--- dist/music.js
+++ dist/music.js
@@ -1,26 +1,21 @@
 const ap = new APlayer({
-  container: document.getElementById('aplayer'),
-  fixed: true,
-  // mini: true,
-  autoplay: true,
-  listFolded: true,
-  preload: 'auto',
-  mutex: true,
-  // theme: '#FADFA3', //列表标签颜色,audio有设置theme这个就会失效
-  loop: 'all',
-  lrcType: 1,
-  // volume: 0.7, //默认音量
-  audio: [{
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
         name: 'ヒカリヘ (向着光) (piano version)',
         artist: '林友树 (林ゆうき)',
-        url: 'https://y.qq.com/#type=song&mid=003g4pMq16jlC7&tpl=yqq_song_detail',
-        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000000ECmTF22VO0h.jpg',
+        url: 'http://music.163.com/song/media/outer/url?id=22676839',
+        cover: 'http://p2.music.126.net/TorVPG2yr7hTlTtqvkTEGg==/109951167783660809.jpg',
       },
       {
-        name: '雨空)',
-        artist: 'α·Pav',
-        url: 'https://music.163.com/#/song?id=139206',
+        name: '雨空',
+        artist: '羽肿α·Pav',
+        url: 'http://music.163.com/song/media/outer/url?id=139206',
         cover: 'http://p3.music.126.net/tBTNafgjNnTL1KlZMt7lVA==/18885211718935735.jpg',
       }
-  ]
+    ]
 });
+
+
